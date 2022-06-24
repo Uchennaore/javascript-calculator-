095d2e49906e048f3644f1e3e48eda78e0c3f354
Uchennaore <64827412+Uchennaore@users.noreply.github.com>
2022-06-24 16:55:16 +0100

    first commit

---
+++ hello.js
@@ -0,0 +1,34 @@
+const prompt = require('prompt-sync')();
+
+let number1 = parseFloat(prompt("Enter first number:"));
+let number2 = parseFloat(prompt("Enter second number:"));
+let operation = prompt("Enter operation:  *, +, -, /:")
+
+ if(operation == "*"){
+    console.log(number1 *  number2)
+ }  else if(operation == "+"){
+    console.log(number1 +  number2)
+ }  else if(operation == "-"){
+    console.log(number1 -  number2)
+ }  else if(operation == "/"){
+    console.log(number1 /  number2)
+ }
+//function calculator(number1, number2, operation) {
+//    switch (operation) {
+//        case "+":
+//            alert(number1 + number2)
+//            break;
+//        case "-":
+//            alert(number1 - number2)
+//            break;
+//       case "*":
+//            alert(number1 * number2)
+//            break;
+//        case "/":
+//            alert(number1 / number2)
+//           break;
+//        default:
+//            return 'Please enter 2 valid numbers and an operation in this calculator'
+//    }
+//} 
+
